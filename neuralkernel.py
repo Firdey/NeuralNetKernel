Code runs but incorrect result because the loss function increases, SAheart data for binary classification has been added.

neuralkernel.py
@@ -51,33 +51,32 @@ class Network:
 
         #ADDITIONAL MEMBER VARIABLES
         self.m = layer_size[1] #size of hidden layer
-        self.weights = np.ones( ( self.m , self.p ) ) * 0.05 #matrix of angular frequncies (m x p matrix)
-        self.betas = np.ones( 2 * self.m ) * 0.05 #vector of 2m betas (amplitudes)
-        self.loss_vector = np.zeros( max_it ) #the loss at each step of backward propagation
+        self.weights = np.random.normal(0.0, 10, (self.m,self.p)) #matrix of angular frequncies (m x p matrix)
+        self.betas = np.random.normal(0.0, 10, 2*self.m) #vector of 2m betas (amplitudes)
+        self.loss_vector = np.zeros(max_it) #the loss at each step of backward propagation
         self.iterations = 0  #number of iterations done so far
 
-        self.weight_diff = 0 #for momentum
-        self.betas_diff = 0 #for momentum
-        self.grad_w_loss = np.zeros ((self.m , self.p )) #THIS SHOULD BE IN BACKWARD METHOD
+        self.weight_diff = np.zeros((self.m,self.p)) #for momentum
+        self.betas_diff = np.zeros(2*self.m) #for momentum
 
-        self.phi_matrix = 0
-        self.weight_vector = 0
-        self.ydata_hat = 0
+        self.phi_matrix = np.zeros((2*self.m, self.n))
+        self.ydata_hat = np.zeros(self.n)
+        self.weight_vector = np.zeros( ( self.m , self.p ) )
 
     #METHODS
 
-    #FORWARD PROPAGATION
+    #METHOD: FORWARD PROPAGATION
         #given the parameters predict the response vectors
     def forward( self ):
         xdata_weight = np.dot(self.weights, self.xdata.transpose()) #this is  dim m X p of weighted data
-        self.phi_sin = np.sin(xdata_weight) # sin part of phi
-        self.phi_cos = np.cos(xdata_weight) # cos part of phi
-        self.phi_matrix = np.vstack((self.phi_cos, self.phi_sin)) / np.sqrt(self.m) # stack cosines on top of sines
-        weight_vector = np.dot(self.betas, self.phi_matrix) # dim n  includes paramters inside the activation function
-        self.ydata_hat = self.activ.formula(weight_vector) # the fitted y's that depend on the activation fn
+        phi_sin = np.sin(xdata_weight) # sin part of phi
+        phi_cos = np.cos(xdata_weight) # cos part of phi
+        self.phi_matrix = np.vstack((phi_cos, phi_sin)) / np.sqrt(self.m) # stack cosines on top of sines
+        self.weight_vector = np.dot(self.betas, self.phi_matrix) # dim n  includes paramters inside the activation function
+        self.ydata_hat = self.activ.formula(self.weight_vector) # the fitted y's that depend on the activation fn
 
 
-    #BACKWARD PROPAGATION
+    #METHOD: BACKWARD PROPAGATION
         #given the response vectors, update weights and betas
     def backward ( self ):
         #loss vector is the evaluation of our objective function in every iteration
@@ -97,45 +96,53 @@ class Network:
 
         #LAYER 2 - UPDATE WEIGHTS
 
-        betas_cos = self.betas[0 : self.m]
-        betas_sin = self.betas[self.m : 2 * self.m]
+        #set the step size due to momentum
+        self.weight_diff *= self.alpha #momentum term
 
         #for each column in the weight matrix, update the weights using gradient descent
         for j in range(0, self.m):
 
             #gradient of objective wrt weight vector = X' * (diff_loss .* diff_activation .* diff_fourier)
-            #where diff_four is a n vector with elements (beta_sin * cos(weight*x) - beta_cos * sin(weight*x))
+            #where diff_four is a n vector with elements (beta_sin[j] * cos(weight*x) - beta_cos[j] * sin(weight*x))
 
-            grad = (betas_sin[j]*self.phi_matrix[j,:] - betas_cos[j]*self.phi_matrix[j+ self.m,:])
-            grad *= s_grad
-            grad *= loss_grad
+            grad_w_loss = self.betas[j+self.m]*self.phi_matrix[j,:] - self.betas[j]*self.phi_matrix[j+self.m,:]
+            grad_w_loss *= s_grad
+            grad_w_loss *= loss_grad
+            grad_w_loss = np.dot(self.xdata.transpose(),grad_w_loss)
+            grad_w_loss += 2*self.lam*self.weights[j, :]
 
-            #this gives a p by n matrix where each data point i is scaled by the appropiate coef
-            help_mat =  - self.loss.diff(self.ydata, self.ydata_hat, self.n) * s_grad * ( self.phi_cos[ j, : ] *  betas_sin[ j ] - self.phi_sin[ j, : ] *  betas_cos[ j ])* self.xdata.transpose()
+            #CODE FROM XENIA
             #now we sum the columns of the matrix to obtain a p vector for the gradient wrt w_r
-            term_1 = np.array( [sum(column) for column in help_mat]).transpose()
+            #term_1 = np.array( [sum(column) for column in help_mat]).transpose()
 
-            self.grad_w_loss[ j ] = 2 * self.lam * self.weights[ j ] + term_1
-            self.weight_diff = (self.epsilon * self.grad_w_loss + self.alpha * self.weight_diff)
-            self.weights = self.weights - self.weight_diff  
+            #append most recent gradient
+            self.weight_diff[j, :] += self.epsilon * grad_w_loss
+
+        #end j for
+
+        #take the step and increment the number of iterations
+        self.weights -= self.weight_diff
         self.iterations += 1
 
+    #end def backward(self)
 
-    def train ( self ):
-        self.forward()
-        self.backward()
-        while (self.iterations < max_it ) or (self.loss_vector[self.iterations -1] <= self.tolerance):
-       # for h in range( 0, max_it):
+    #METHOD: TRAIN NEURAL NETWORK
+      #iterate between forward and backward pass
+    def train (self):
+        while (self.iterations < self.max_it):
             self.forward()
             self.backward()
+            if self.loss_vector[self.iterations-1] < self.tolerance:
+              break
 
+#ACTIVATION STATIC CLASSES
 class Sigmoid:
-        @staticmethod
-        def formula(z):
-            return 1/(1+np.exp(-z))
-        @staticmethod
-        def diff(z):
-            return Sigmoid.formula(z) * (1-Sigmoid.formula(z))
+    @staticmethod
+    def formula(z):
+        return 1/(1+np.exp(-z))
+    @staticmethod
+    def diff(z):
+        return Sigmoid.formula(z) * (1-Sigmoid.formula(z))
 class Softmax:
     @staticmethod
     def formula(z):
@@ -143,7 +150,15 @@ class Softmax:
     @staticmethod
     def diff(z):
         return
+class Linear:
+    @staticmethod
+    def formula(z):
+        return z
+    @staticmethod
+    def diff(z):
+        return np.ones(len(z))
 
+#LOSS STATIC CLASSES
 class Mean_S_E:
     @staticmethod
     def formula(ydata, ydata_hat, n):
@@ -155,37 +170,65 @@ class Mean_S_E:
         return 2 * y_errors * 1.0 / ( n )
 
 
-# Copy into other code to run directly!!!! Though has bugs!!!
 import numpy as np
+import math
 import csv
+
+'''
+print "Importing data..."
 with open('forestfiretrans.csv', 'rb') as csvfile:
     next(csvfile, None)
     forest = csv.reader(csvfile)
     forest_data = [map(float, l) for l in forest]
-    print forest_data
-    #for row in forest:
-for i in range(0, 517):
-    forest_data[i].pop(0)
+'''
 
-yforest = [0 for i in range(517)]
-for j in range(0,517):
-    yforest[j] = forest_data[j][8]
-    forest_data[j].pop(8)
-print yforest
-print forest_data
+#IMPORT DATA
+print "Importing data..."
+with open('SAheart.csv', 'rb') as csvfile:
+    next(csvfile, None)
+    data_matrix = csv.reader(csvfile)
+    data_matrix = [map(float, l) for l in data_matrix]
 
-layer_size = [8, 50, 1]
-loss = Mean_S_E
-activ = Sigmoid
-xdata = np.array(forest_data)
-ydata = np.array(yforest)
-epsilon = 0.5
-alpha = 0.05
-penalty = [0, 0]
-max_it = 1000
-tolerance = 0.5
-Forest_neural = Network(layer_size, loss, activ, xdata, ydata, epsilon, alpha, penalty, max_it, tolerance)
-Forest_neural.train()
-print Forest_neural.loss_vector[1:100]
-print Forest_neural.ydata_hat
-print ydata
+#GET DIMENSION OF DATA AND PRINT IT
+n = len(data_matrix)
+p = len(data_matrix[0])-1
+print "Data set has "+str(n)+" samples and "+str(p)+" features"
+
+#EXTRACT THE RESPONSE VARAIBLE FROM DATA MATRIX AND REMOVE IT
+#ADD CONSTANT (BIAS) TERM
+y_response = [0]*n #y_response is a n vector
+#for each data
+for i in range(0, n):
+    #change the row name into a constant
+    data_matrix[i][0]=1.0
+    #extract the ith response and put it in the y_response vector
+    y_response[i] = data_matrix[i][p]
+    #remove the response from the data matrix
+    #pop(p) = remove the pth element
+    data_matrix[i].pop(p)
+
+#DECLARE VARIABLES/OBJECTS FOR NEURAL NETWORK
+layer_size = [p, 100, 1] #size 3 vector [number of features, number of hidden nodes, dimension of response variable]
+loss = Mean_S_E #static class with a method for the loss and its differential
+activ = Sigmoid #static class with a method for the activation function and its differential
+X = np.array(data_matrix) #design matrix as a numpy array
+y = np.array(y_response) #response vector as a numpy array
+epsilon = 0.0001 #step size
+alpha = 0.0 #coefficient for momentum
+penalty = [0.0, 0.0] #size 2 vector for ridge regression tuning parameters for [betas (amplitudes), weights (angular frequencies)]
+max_it = 1000 #number of iterations
+tolerance = 0.0 #tolerence
+
+#TRAIN THE NEURAL NETWORK
+print "Training classifier..."
+neural_network = Network(layer_size, loss, activ, X, y, epsilon, alpha, penalty, max_it, tolerance)
+neural_network.train()
+
+#PRINT THE RESULTS
+print "Below are the feature vectors, responses and its prediction for each data"
+for i in range(0,n):
+    print "Feature"+str(data_matrix[i])+" Response: "+str(y[i])+" Prediction: "+str(neural_network.ydata_hat[i])
+error = np.sum(np.absolute(y-neural_network.ydata_hat))*100/n
+print "Test error: "+str(error)
+print "Loss function at every iteration"
+print neural_network.loss_vector